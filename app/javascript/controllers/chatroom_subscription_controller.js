91995bce1e749d0601d51b3269b6dcb1e92c70ee
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2022-11-11 18:23:44 +0100

    feat: establish subscription from client

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -1,7 +1,15 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
+import { createConsumer } from "@rails/actioncable";
 
 // Connects to data-controller="chatroom-subscription"
 export default class extends Controller {
+  static values = { chatroomId: Number };
+  static targets = ["messages"];
+
   connect() {
+    // Create a consumer object
+    const consumer = createConsumer();
+    // That consumer will subscribe to the ChatroomChannel for any signals
+    consumer.subscriptions.create("ChatroomChannel");
   }
 }
